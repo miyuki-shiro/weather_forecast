3104fd9e5dcb76a3e17d90c0b97a53a2652c3e66
miyuki_shiro <thaimyg@yahoo.cl>
2019-12-08 01:20:26 -0300

    Url error fixed again xD

--- src/config.js
+++ src/config.js
@@ -3,6 +3,6 @@ const API_KEY = '1f6687dfd7a2fa477c3a4c2859df0a4e';
 const CURRENT_WEATHER_BASE_URL = `https://api.openweathermap.org/data/2.5/weather?q=`;
 const CURRENT_WEATHER_LAST_URL = `,cl&units=metric&appid=${API_KEY}`;
 
-const CURRENT_FORECAST_BASE_URL = `http://api.openweathermap.org/data/2.5/forecast?q=`;
+const CURRENT_FORECAST_BASE_URL = `https://api.openweathermap.org/data/2.5/forecast?q=`;
 
 export { API_KEY, CURRENT_WEATHER_BASE_URL, CURRENT_WEATHER_LAST_URL, CURRENT_FORECAST_BASE_URL }; 
