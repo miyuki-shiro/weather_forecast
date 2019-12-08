2ca7297b144d5a1cc9a17aaf428489cfde6cd5a3
miyuki_shiro <thaimyg@yahoo.cl>
2019-12-08 01:18:17 -0300

    Url error fixed

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const API_KEY = '1f6687dfd7a2fa477c3a4c2859df0a4e';
-//const CURRENT_WEATHER = `http://api.openweathermap.org/data/2.5/weather?q=${PLACE},cl&?units=metric&appid=${API_KEY}`;
-const CURRENT_WEATHER_BASE_URL = `http://api.openweathermap.org/data/2.5/weather?q=`;
+//const CURRENT_WEATHER = `https://api.openweathermap.org/data/2.5/weather?q=${PLACE},cl&?units=metric&appid=${API_KEY}`;
+const CURRENT_WEATHER_BASE_URL = `https://api.openweathermap.org/data/2.5/weather?q=`;
 const CURRENT_WEATHER_LAST_URL = `,cl&units=metric&appid=${API_KEY}`;
 
 const CURRENT_FORECAST_BASE_URL = `http://api.openweathermap.org/data/2.5/forecast?q=`;
